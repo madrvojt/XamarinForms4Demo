ae9f7d7ee364ba8dff5f8efb580571afe70057f7
Vojtěch Mádr <madrvojtech@outlook.com>
2019-10-31 21:42:49 +0100

    initial commit

---
+++ XamarinForms4Demo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinForms4Demo.Models;
+
+namespace XamarinForms4Demo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
